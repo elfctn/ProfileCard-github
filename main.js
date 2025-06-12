7a7d51b2b0151ccc931a836971d7e2caf9c47193
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-12 16:48:03 +0300

    DOM elements added

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 const API_URL = "https://api.github.com/users/";
+
+const form = document.getElementById("form"); //Kullanıcı adını girmek için.
+const search = document.getElementById("search"); // Arama input alanı seçilir.
+const main = document.getElementById("main"); //Kullanıcı kartının gösterileceği ana kapsayıcı seçilir.
